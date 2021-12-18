6f7af02d719f0d6592d6e0943b5d52d0ed774b5a
yagosant <yagojf27@gmail.com>
2021-12-18 14:41:59 -0300

    feature/ criar o alert de confirmação da msg

--- src/components/BtnDelContasReceber/BtnDelContasReceber.tsx
+++ src/components/BtnDelContasReceber/BtnDelContasReceber.tsx
@@ -1,11 +1,50 @@
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 export default function BtnDelContasReceber(){
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+      };
+    
+      const handleClose = () => {
+        setOpen(false);
+      };
+
     return (
+        <div>
         <Stack spacing={2} direction="row">
-          <Button variant="contained" color="error" onClick={() => { alert('Excluido') }}>Excluir</Button>
+          <Button variant="contained" color="error" onClick={handleClickOpen}>Excluir</Button>
         </Stack>
+
+        <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          <b>{"Atenção!"}</b>
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+           Deseja realmente <b>Excluir</b> esse registro?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color = 'error'>Cancelar</Button>
+          <Button onClick={handleClose} autoFocus>
+            Continuar
+          </Button>
+        </DialogActions>
+      </Dialog>
+        </div>
       );
 }
