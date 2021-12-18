f069ec0a2afe02565a9eaab6d3574969127acf72
Nycolas Lacerda <nycolas.lacerda@rbmweb.com.br>
2021-12-18 14:37:03 -0300

    feat: ControleContas

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,21 +1,18 @@
-import { useEffect } from "react"
-import { setPlanoDeContas } from "../../models/firestore/PlanoDeContasStore"
+import { getPlanoDeContas } from "../../models/firestore/PlanoDeContasStore"
 import PlanoDeContas from "../../models/PlanoDeContas"
+import EnhancedTable from '../ContasAPagar/ControleContas';
 
 export default function Home(){
-    useEffect(
-        () => {
-            setPlanoDeContas(
-                new PlanoDeContas('teste', 'teste', true)
-            )
-        }, []
-    )
+    const planoDeContas: PlanoDeContas = getPlanoDeContas();
+    
+    
     return (
         <div>
             <p>
                 Esta é a tela que será utilizada para os exercícios ao longo do treinamento. Abaixo dessa tag faça a inserção dos seus componentes...
             </p>
             <hr />
+            <EnhancedTable planoDeContas={planoDeContas} />
         </div>
     )
 }
