0fe438b8fc2521a9f8b26d656d6c5dda13aeb695
otavioaufegues <otavioaufegues@gmail.com>
2021-12-18 15:36:03 -0300

    promisse to array , 1/10

--- src/components/Receitas/Receitas.tsx
+++ src/components/Receitas/Receitas.tsx
@@ -1,45 +1,40 @@
-import * as React from 'react';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import * as React from "react";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import { getReceitas } from "../../models/firestore/ReceitasStore";
+import { IReceitas } from "../../models/interfaces/IReceitas";
 
 const columns: GridColDef[] = [
-  { field: 'descricao', headerName: 'Descrição', width: 90 },
+  { field: "descricao", headerName: "Descrição", width: 90 },
   {
-    field: 'data',
-    headerName: 'Data',
+    field: "data",
+    headerName: "Data",
     width: 150,
   },
   {
-    field: 'status',
-    headerName: 'Status',
+    field: "status",
+    headerName: "Status",
     width: 150,
   },
   {
-    field: 'plano',
-    headerName: 'Plano de Contas',
+    field: "plano",
+    headerName: "Plano de Contas",
     width: 110,
   },
   {
-    field: 'valor',
-    headerName: 'Valor',
-    width: 160
+    field: "valor",
+    headerName: "Valor",
+    width: 160,
   },
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
-
 export default function Receitas() {
+  const [rows, setRows] = React.useState([]);
+
+  async () => {
+    setRows(getReceitas());
+  };
   return (
-    <div style={{ height: 400, width: '100%' }}>
+    <div style={{ height: 400, width: "100%" }}>
       <DataGrid
         rows={rows}
         columns={columns}
