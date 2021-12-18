c9ba396d3690096720838475f4a8b32808fc266b
IcaroAndries <icaro.andries15@gmail.com>
2021-12-18 16:34:42 -0300

    feature: plano de contas

---
+++ src/components/AddPlanoConta/AddPlanoConta.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { setPlanoDeContas } from "../../models/firestore/PlanoDeContasStore";
+import PlanoDeContas from "../../models/PlanoDeContas"
+
+export default function AddPlanoConta() {
+    
+    // const [seguraDados, attDados] = useState(['',0,'']);
+    
+    function funcao1()
+    {
+        alert("Plano de Conta adicionado com Sucesso!")
+    }
+    const [seguraCod, attCod] = useState('');
+    const [seguraConta, attConta] = useState('');
+    const [seguraStatus, attStatus] = useState(false);
+
+    const opcao = [{value : true, label : "True"},{value : false, label : "False"}]
+
+    const atualiza = () => {
+        setPlanoDeContas(new PlanoDeContas(seguraCod, seguraConta, seguraStatus));
+    }
+
+    return(
+        <div>
+            <label>Código da Conta</label>
+            <br></br>
+            <input type = "text" name="EditCodigo" value={seguraCod} onChange={(e)=>attCod(e.target.value)} placeholder='Digite o Código da Conta'></input>
+
+            <br></br><br></br>
+            <label>Nome da Conta</label>
+            <br></br>
+            <input type = "text" name="EditNome" value={seguraConta} onChange={(e)=>attConta(e.target.value)} placeholder='Digite o Nome da Conta'></input>
+
+            <br></br><br></br>
+            <label>Status da Conta</label>
+            <br></br>
+            <select name="select" >
+                <option value="valor1" selected>True</option>
+                <option value="valor2">False</option>
+            </select>
+            {/* <input type = "text" placeholder='Digite o Tipo da Conta'></input> */}
+
+            <br></br><br></br>
+
+            <input type = "button" onClick={atualiza} value={"              Adicionar              "}/>
+
+        </div>
+
+    )
+}
