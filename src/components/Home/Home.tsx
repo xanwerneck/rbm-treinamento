3696273ebd96294e89f93ff4dada4fd952090e62
otavioaufegues <otavioaufegues@gmail.com>
2021-12-18 14:04:58 -0300

    add data grid e mostra grid inicial

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react"
 import { setPlanoDeContas } from "../../models/firestore/PlanoDeContasStore"
 import PlanoDeContas from "../../models/PlanoDeContas"
+import Receitas from "../Receitas/Receitas";
 
 export default function Home(){
     useEffect(
@@ -12,10 +13,7 @@ export default function Home(){
     )
     return (
         <div>
-            <p>
-                Esta é a tela que será utilizada para os exercícios ao longo do treinamento. Abaixo dessa tag faça a inserção dos seus componentes...
-            </p>
-            <hr />
+            <Receitas />
         </div>
     )
 }
