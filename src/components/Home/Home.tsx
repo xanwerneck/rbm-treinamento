1f55f03a1e02288d950aa511a54216da6e1f9c4e
Nycolas Lacerda <nycolas.lacerda@rbmweb.com.br>
2021-12-18 17:55:16 -0300

    feat: icon delete

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,3 @@
-import { getPlanoDeContas } from "../../models/firestore/PlanoDeContasStore"
-import PlanoDeContas from "../../models/PlanoDeContas"
 import EnhancedTable from '../ContasAPagar/ControleContas';
 
 export default function Home(){
@@ -8,10 +6,6 @@ export default function Home(){
     
     return (
         <div>
-            <p>
-                Esta é a tela que será utilizada para os exercícios ao longo do treinamento. Abaixo dessa tag faça a inserção dos seus componentes...
-            </p>
-            <hr />
             <EnhancedTable />
         </div>
     )
---
+++ src/models/interfaces/IContasAPagar.ts
@@ -0,0 +1,7 @@
+export interface IContasAPagar {
+    descricao : string
+    data : Date
+    valor: number
+    status : boolean
+    planodecontas: string
+}
